0b14d44831087ce60ca878a0d09d862866d14be4
PedroCoutoCampos <96892535+PedroCoutoCampos@users.noreply.github.com>
2022-01-18 18:00:09 -0300

    atualizar lista 4

---
+++ Semana 04/LISTA 4/ex11.js
@@ -0,0 +1,13 @@
+/* 
+11 -[REFATORAÇÃO]Crie um algoritmo que tem como
+entrada um array destrings e trata essas string 
+substituindo os números por letra de acordo com a
+tabela a seguir, além disso, deve remover os espaços 
+em branco no começo efim da string, se existirem.
+Tabela: 1 : i3 : e4 : a5 : s0 : o 
+Exemplo entrada:
+['  h3ll0 w0rld', '  w3b d3v3l0p3r  ', '0tterw1s3', 'j4v4scr1pt ']
+Exemplo Saída:
+[ 'helloworld', 'webdeveloper', 'otterwise', 'javascript' ]
+*/
+
