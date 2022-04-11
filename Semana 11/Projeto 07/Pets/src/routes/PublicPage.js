99adf64355a03b21acfaac135d81156dcf6360c3
PedroCoutoCampos <pedrocoutocampos@outlook.com>
2022-04-11 20:28:21 -0300

    :fire: Adiconando página pública

--- Semana 11/Projeto 07/Pets/src/routes/PublicPage.js
+++ Semana 11/Projeto 07/Pets/src/routes/PublicPage.js
@@ -1,67 +1,36 @@
-import { Table } from 'react-bootstrap'
-import React from 'react';
+import { useState, useEffect } from "react";
 
+import { listPets } from "../services/pets";
 
+export default function Listarpets() {
+  const [listarPets, setlistarPets] = useState([]);
+  useEffect(() => {
+    const request = async () => {
+      const response = await listPets();
+      setlistarPets(response.data.pets);
+    };
+    request();
+  }, []);
 
-class PublicPage extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    // eslint-disable-next-line react/no-direct-mutation-state
-    this.state = {
-      pets: []
-    }
-  }
-  componentDidMount() {
-    this.buscarPets();
-  }
-  componentWillUnmount() {
-
-  }
-  buscarPets = () => {
-    fetch("https://otterwise-fake-api.herokuapp.com/pets/0o5fh3a")
-      .then(response => response.json())
-      .then(dados => {
-        this.setState({ pets: dados })
-      })
-  }
-
-
-  render() {
-    return (
-      <Table striped bordered hover size="sm">
-        <thead> {/* Cabeçario  */}
-          <tr>
-            <th>Name</th>
-            <th>Breed</th>
-            <th>Age</th>
-            <th>Species</th>
-            <th>Gender</th>
-            <th>Url</th>
-
-          </tr>
-        </thead>
-        <tbody>
-
-
-          <tr>{/* linha */}
-
-            <td>{this.state.pets.Name}</td>
-            <td>{this.state.pets.Breed}</td>
-            <td>{this.state.pets.Age}</td>
-            <td>{this.state.pets.Species}</td>
-            <td>{this.state.pets.Gender}</td>
-            <td>{this.state.pets.Url}</td>
-          </tr>
-
-
-        </tbody>
-      </Table>
-    )
-
-  }
+  return (
+    <div>
+      {listarPets !== [] ? (
+        listarPets.map(({ id, name, breed, age, species, gender, url }) => (
+          <div key={id}>
+            <img src={url} alt={breed} />
+            <div>
+              <h3>
+                {name} - {age} ano(s)
+              </h3>
+              <p>
+                {species} - {gender}
+              </p>
+            </div>
+          </div>
+        ))
+      ) : (
+        <h1>Aguarde...</h1>
+      )}
+    </div>
+  );
 }
-
-
-export default PublicPage
